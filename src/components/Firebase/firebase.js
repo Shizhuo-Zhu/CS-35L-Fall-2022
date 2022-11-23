99f0ff2ef9adcf4c68d4322dd838e4ee01335da7
eyoon1131 <eyoon1131@gmail.com>
2022-11-22 22:00:18 -0800

    Created basic signup/login functionality

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,25 +1,70 @@
-// Import the functions you need from the SDKs you need
+import { Google } from "@mui/icons-material";
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  GoogleAuthProvider, getAuth, signInWithPopup, signInWithEmailAndPassword,
+  createUserWithEmailAndPassword, sendPasswordResetEmail, signOut,
+} from 'firebase/auth';
+import {
+  getFirestore, query, getDocs, collection, where, addDoc,
+} from "firebase/firestore";
 
-//import app from 'firebase/app';
-import {getAuth} from 'firebase/auth';
-
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCgTTxgLjInbnDE95PxPufk6jRMNhz0KGs",
-  authDomain: "cs35l-deb37.firebaseapp.com",
-  projectId: "cs35l-deb37",
-  storageBucket: "cs35l-deb37.appspot.com",
-  messagingSenderId: "695336358121",
-  appId: "1:695336358121:web:4323e9d437fc8c1f1f6078"
+  apiKey: "AIzaSyCZtpVNAOIop_HtPfHrv2YPhJirFOAFvW8",
+  authDomain: "cs35l-project-99e31.firebaseapp.com",
+  projectId: "cs35l-project-99e31",
+  storageBucket: "cs35l-project-99e31.appspot.com",
+  messagingSenderId: "954504558969",
+  appId: "1:954504558969:web:e3189df5a563b5f40dd7a8",
+  measurementId: "G-H2WEKP7DRL"
 };
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-export {auth}
+const db = getFirestore(app);
+
+const logIn = async(email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const register = async(name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const passwordReset = async(email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Link to reset password sent");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export {
+  auth, db, logIn, register, passwordReset, logout,
+};
+
+
 
 
