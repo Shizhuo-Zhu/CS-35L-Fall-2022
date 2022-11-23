21f7314eef3c675e739ef6ea7a8d4b630cfe0487
RyanWang3 <ryan.rh.wang@gmail.com>
2022-11-23 03:49:01 -0800

    Added backgroundImage to HomePage
    added backgroundImage into /public folder
    added background image onto the HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,21 @@
-import { Button, CssBaseline } from "@mui/material";
+import { Box, Button, CssBaseline } from "@mui/material";
 import React from "react";
 import Navbar from "../components/Navbar";
-
 const HomePage = () => {
   return (
     <div>
       <Navbar />
+      <Box
+        class="bg"
+        style={{
+          backgroundImage: `url(${
+            process.env.PUBLIC_URL + "/backgroundImage.jpg"
+          })`,
+          backgroundSize: "cover",
+          height: "100vh",
+          color: "#f5f5f5",
+        }}
+      ></Box>
       <CssBaseline />
     </div>
   );
