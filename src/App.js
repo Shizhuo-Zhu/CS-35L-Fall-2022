0c3917fb9218b88f47892626493a460ee861c62c
RyanWang3 <ryan.rh.wang@gmail.com>
2022-11-23 01:59:38 -0800

    Created HomePage Navbar
    Created Navbar with login button and sidebar button
    Created new route in App.js for HomePage
    Added Font styling in index.html

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import './App.css';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import DashBoard from './pages/DashBoard';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React from "react";
+import "./App.css";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import DashBoard from "./pages/DashBoard";
+import HomePage from "./pages/HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignIn />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<DashBoard/>}/>
+        <Route path="/dashboard" element={<DashBoard />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import { Button, CssBaseline } from "@mui/material";
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <CssBaseline />
+    </div>
+  );
+};
+
+export default HomePage;
