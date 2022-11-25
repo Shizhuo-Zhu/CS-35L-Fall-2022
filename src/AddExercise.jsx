18a94d78f975a6a80e8ae465550cc84e2cfb7d8e
Aneesh B <aneeshbonthala15@gmail.com>
2022-11-25 02:01:02 -0800

    Full functionality for add exercise. Only need to add styling.

--- src/AddExercise.jsx
+++ src/AddExercise.jsx
@@ -1,142 +1,145 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
 import InputLabel from '@mui/material/InputLabel';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import InputBase from '@mui/material/InputBase';
-import { styled } from '@mui/material/styles';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
-
-const BootstrapInput = styled(InputBase)(({ theme }) => ({
-    'label + &': {
-      marginTop: theme.spacing(3),
-    },
-    '& .MuiInputBase-input': {
-      borderRadius: 4,
-      position: 'relative',
-      backgroundColor: theme.palette.background.paper,
-      border: '1px solid #ced4da',
-      fontSize: 16,
-      padding: '10px 26px 10px 12px',
-      transition: theme.transitions.create(['border-color', 'box-shadow']),
-      // Use the system font instead of the default Roboto font.
-      fontFamily: [
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-      ].join(','),
-      '&:focus': {
-        borderRadius: 4,
-        borderColor: '#80bdff',
-        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-      },
-    },
-  }));
-
-  function SetFeatures() {
+  function SetFeatures(props) {
+    const unit = props.weightUnit;
+    const [weight, setWeight] = React.useState('');
+    const [reps, setReps] = React.useState('');
+    const [notes, setNotes] = React.useState('');
+    const handleWeightChange = event => {
+        const result = event.target.value.replace(/\D/g, '');
+        setWeight(result);
+    };
+    const handleRepsChange = event => {
+        const result = event.target.value.replace(/\D/g, '');
+        setReps(result);
+    };
     return (
     <div>
-        <FormControl sx={{ m: 1, width: 100}} variant="filled">
-            <InputLabel htmlFor="weight">Weight</InputLabel>
-            <BootstrapInput id="weight" />
+        <FormControl sx={{ m: 1, width: '11ch' }} variant="outlined">
+          <OutlinedInput
+            id="weight"
+            type="text"
+            value= {weight}
+            onChange={handleWeightChange}
+            endAdornment={<InputAdornment position="end">{unit}</InputAdornment>}
+            inputProps={{
+              'aria-label': 'weight',
+              maxLength: 4,
+            }}
+            required
+          />
+          <FormHelperText id="weight">Weight</FormHelperText>
         </FormControl>
-        <FormControl sx={{ m: 1 , width: 75}} variant="filled">
-            <InputLabel htmlFor="reps">Reps</InputLabel>
-            <BootstrapInput id="reps" />
+        <FormControl sx={{ m: 1, width: '5.5ch' }} variant="outlined">
+          <OutlinedInput
+            id="reps"
+            value= {reps}
+            onChange={handleRepsChange}
+            inputProps={{
+              'aria-label': 'reps',
+              maxLength: 2
+            }}
+            required
+          />
+          <FormHelperText id="reps">Reps.</FormHelperText>
         </FormControl>
-        <FormControl sx={{ m: 1 , width: 300}} variant="filled">
-            <InputLabel htmlFor="notes">Notes</InputLabel>
-            <BootstrapInput id="notes" />
+        <FormControl sx={{ m: 1, width: '35ch' }} variant="outlined">
+          <OutlinedInput
+            id="notes"
+            value= {notes}
+            onChange={(e) => setNotes(e.target.value)}
+            inputProps={{
+              'aria-label': 'notes',
+              maxLength: 50
+            }}
+          />
+          <FormHelperText id="notes">Notes</FormHelperText>
         </FormControl>
     </div>
     );
 }
 
 function AddNewExercise() {
+  const [name, setName] = React.useState('');
   const [sets, setSets] = React.useState(1);
-  const handleChange = (event) => {
-    setSets(event.target.value);
-  };
+  const [unit, setUnit] = React.useState('lbs');
   let displaySetFeatures = [];
   for (let i = 0; i < sets; i++) {
-      displaySetFeatures.push(<SetFeatures key={i}/>)
+      displaySetFeatures.push(<SetFeatures key={i} weightUnit={unit}/>)
   }
   return (
     <div>
-      <FormControl sx={{ m: 1 }} variant="filled">
-        <InputLabel htmlFor="name">Name</InputLabel>
-        <BootstrapInput id="name" />
-      </FormControl>
-      <FormControl sx={{ m: 1 }} variant="filled">
+        <FormControl sx = {{m: 1, width: '30ch'}}>
+        <TextField id="name"
+                   label="Name"
+                   variant="outlined"
+                   onChange={(e) => setName(e.target.value)}
+                   inputProps={{
+                       maxLength: 30
+                   }}
+                   required
+                />
+        </FormControl>
+      <FormControl sx={{ m: 1 }} variant="standard">
         <InputLabel id="sets">Sets</InputLabel>
        <Select
           labelId="sets"
           id="sets"
           value={sets}
-          onChange={handleChange}
-          input={<BootstrapInput />}
+          onChange={(e) => setSets(e.target.value)}
         >
           <MenuItem value={1}>1</MenuItem>
           <MenuItem value={2}>2</MenuItem>
           <MenuItem value={3}>3</MenuItem>
           <MenuItem value={4}>4</MenuItem>
           <MenuItem value={5}>5</MenuItem>
+          <MenuItem value={6}>6</MenuItem>
+          <MenuItem value={7}>7</MenuItem>
+          <MenuItem value={8}>8</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl sx={{ m: 1 }} variant="standard">
+        <InputLabel id="unit">Unit</InputLabel>
+       <Select
+          labelId="unit"
+          id="unit"
+          value={unit}
+          onChange={(e) => setUnit(e.target.value)}
+        >
+          <MenuItem value={'lbs'}>lbs</MenuItem>
+          <MenuItem value={'kg'}>kg</MenuItem>
         </Select>
       </FormControl>
       {displaySetFeatures}
-    </div>
+      </div>
   );
 }
 
+function Log() {
+    return (
+        <Stack direction="row" spacing={2}>
+          <Button variant="outlined">Log</Button>
+        </Stack>
+      );
+}
 
 
-
-export default function DialogSelect() {
-  const [open, setOpen] = React.useState(false);
-  const [Workout, setWorkout] = React.useState('');
-
-  const handleChange = (event) => {
-    setWorkout(Number(event.target.value) || '');
-  };
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = (event, reason) => {
-    if (reason !== 'backdropClick') {
-      setOpen(false);
-    }
-  };
-
-  return (
-    <div>
-      <Button onClick={handleClickOpen}>Add New</Button>
-      <Dialog disableEscapeKeyDown open={open} onClose={handleClose}>
-        <DialogContent>
-          <Box component="form" sx={{ width: 1 }}>
+export default function AddExercise() {
+    return (
+        <div>
             <AddNewExercise/>
-          </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Log</Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
+            <Log/>
+        </div>
+    )
 }
