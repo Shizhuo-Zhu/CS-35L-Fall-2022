9e6922bfc35adc393b8103bd3cb2eaa4b7dad2a1
RyanWang3 <ryan.rh.wang@gmail.com>
2022-11-28 14:42:29 -0800

    Implemented sidebar/added text to HomePage
    Created drawer.jsx which is the sidebar element for project
    added the sidebar element to the Navbar
    added new Text to HomePage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,21 +7,22 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import LoginIcon from "@mui/icons-material/Login";
+import MuiDrawer from "./drawer";
 
 export default function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
+          {/* <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+          > */}
+          <MuiDrawer />
+          {/* </IconButton> */}
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Racer Fitness
           </Typography>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
-import { Box, Button, CssBaseline } from "@mui/material";
+import { AlignHorizontalCenter, CenterFocusStrong } from "@mui/icons-material";
+import { Box, Button, CssBaseline, Typography } from "@mui/material";
 import React from "react";
 import Navbar from "../components/Navbar";
 const HomePage = () => {
   return (
     <div>
       <Navbar />
+
       <Box
         class="bg"
         style={{
@@ -14,8 +16,15 @@ const HomePage = () => {
           backgroundSize: "cover",
           height: "100vh",
           color: "#f5f5f5",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
-      ></Box>
+      >
+        <Typography variant="h1" component="div">
+          Racer Fitness
+        </Typography>
+      </Box>
       <CssBaseline />
     </div>
   );
