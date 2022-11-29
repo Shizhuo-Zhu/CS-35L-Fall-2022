7d4c35e6f10488c97da20150a69d0dbcc40b60a6
Rohith Venkatesh <rvenkatesh2025@ucla.edu>
2022-11-28 22:54:51 -0800

    Database connection test

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,8 +3,25 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import LineChart from '../components/LineChart.jsx';
 import ProfileInfo from '../components/ProfileInfo.jsx';
+import {db} from '../components/Firebase/firebase.js';
+import {
+	query,
+	collection,
+	where,
+	getDocs
+  } from "firebase/firestore";
+
+async function getUser() {
+	const usersRef = collection(db, "users");
+	const q = query(usersRef, where("email", "==", "eggman@cs35l.edu"));
+	const querySnapshot = await getDocs(q);
+	querySnapshot.forEach((doc) => {
+  		console.log(doc.id, " => ", doc.data());
+	});
+}
 
 function Profile() {
+	getUser();
 	return (<>
 				<Box padding={5}>
 					<ProfileInfo />
