445b8c348844302b30d5a3be1d17ee817273503b
RyanWang3 <ryan.rh.wang@gmail.com>
2022-11-29 01:23:40 -0800

    logout button feature implemented
    now when the user is signed in the logout button will appear so user can
    sign out

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,29 +8,75 @@ import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import LoginIcon from "@mui/icons-material/Login";
 import MuiDrawer from "./drawer";
+import { useNavigate } from "react-router";
+import { getAuth, signOut } from "firebase/auth";
 
 export default function Navbar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          {/* <IconButton
+  const navigate = useNavigate();
+  const auth = getAuth();
+  const user = auth.currentUser;
+  if (user) {
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            {/* <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
           > */}
-          <MuiDrawer />
-          {/* </IconButton> */}
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Racer Fitness
-          </Typography>
-          <Button variant="outlined" color="inherit" startIcon={<LoginIcon />}>
-            Login
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+            <MuiDrawer />
+            {/* </IconButton> */}
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Racer Fitness
+            </Typography>
+
+            <Button
+              variant="outlined"
+              color="inherit"
+              startIcon={<LoginIcon />}
+              onClick={() => {
+                signOut(auth);
+                navigate("/Sign-in");
+              }}
+            >
+              Logout
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
+  } else {
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            {/* <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          > */}
+            <MuiDrawer />
+            {/* </IconButton> */}
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Racer Fitness
+            </Typography>
+
+            <Button
+              variant="outlined"
+              color="inherit"
+              startIcon={<LoginIcon />}
+              onClick={() => navigate("/Sign-in")}
+            >
+              Login
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
+  }
 }
