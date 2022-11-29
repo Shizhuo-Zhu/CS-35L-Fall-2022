702e2795059419bf1032b70f28c3b50ab0eb1ea6
Rohith Venkatesh <rvenkatesh2025@ucla.edu>
2022-11-28 21:30:07 -0800

    Added Sample Profile Picture for Initial Visual

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -1,10 +1,16 @@
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import ProfilePic from '../pages/StickProfile.webp';
 
 function ProfileInfo(){
 	return(
 		<Grid container spacing={10}>
+            <Grid item xs={2.3}>
+				<Paper elevation={10}>
+					<img src={ProfilePic} alt="Profile" width="190" height="190"/>
+				</Paper>
+			</Grid>
 			<Grid item xs={5}>
 				<Paper elevation={10}>
 					<Typography variant="p" component="p">
