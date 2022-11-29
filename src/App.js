a2c582eb05bb9bed6f0eb8cb970b501b80a53602
Corey Zhu <shizhuo9806@gmail.com>
2022-11-28 17:25:24 -0800

    Add Calendar page
    make the calendar and notFound page
    really need to figure out firebase now

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React from "react";
-import "./App.css";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import DashBoard from "./pages/DashBoard";
+
+import React from 'react';
+import './App.css';
 import HomePage from "./pages/HomePage";
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import AddExercise from './pages/AddExercise';
+import Schedule from './pages/Calendar';
 import Reset from "./pages/Reset"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotFound from './pages/NotFound';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/calendar" element={<Schedule/>}/>
+        <Route path="/addexercise" element={<AddExercise/>}/>
+        <Route path="/" element={<HomePage/>}/>
         <Route path="/reset" element={<Reset />} />
-        <Route path="/dashboard" element={<DashBoard/>}/>
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </Router>
   );
