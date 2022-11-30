b6bc2509845942ed058893c63c46c1f263756755
Ryan Wang <ryan.rh.wang@gmail.com>
2022-11-29 16:55:52 -0800

    Updated Calendar Page Navbar
    Updated Calendar page to use standard Navbar
    Routed calendar button on sidebar

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -22,6 +22,7 @@ import { useState, useEffect} from 'react';
 import {db} from '../components/firebase.js'
 import {collection, getDocs} from "firebase/firestore";
 import AddExercise from './AddExercise.js';
+import Navbar from '../components/Navbar.jsx';
 
 
 const drawerWidth = 240;
@@ -87,39 +88,10 @@ const Schedule = () => {
   }
   return (
     <ThemeProvider theme={mdTheme}>
+      <Navbar/>
+
       <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppBar position="absolute" open={open}>
-          <Toolbar
-            sx={{
-              pr: '24px', // keep right padding when drawer closed
-            }}
-          >
-            
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: '36px',
-                ...(open && { display: 'none' }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-        
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-                Exercise Log
-            </Typography>
-          </Toolbar>
-        </AppBar>
         <Box
           component="main"
           sx={{
