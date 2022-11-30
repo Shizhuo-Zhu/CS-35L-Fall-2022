d6db243cbf143f8f95a46ef0560264f377275dd8
Rohith Venkatesh <rvenkatesh2025@ucla.edu>
2022-11-30 15:20:06 -0800

    Profile Page with Firebase Access

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -1,12 +1,41 @@
+import React, {useState, useEffect} from 'react';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import ProfilePic from '../pages/StickProfile.webp';
+import { auth } from "../components/firebase.js";
+import { db } from "../components/Firebase/firebase.js";
+import { query, collection, getDocs, where } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
 
 function ProfileInfo(){
+	
+	const defaultData = {"name": "John Doe",
+	                     "email": "johndoe@somecompany.com",
+						};
+
+	const [data, setData] = useState(defaultData);
+	useEffect(() => {
+		onAuthStateChanged(auth, async (user) => {
+			if (user) {
+				let info = {};
+				const usersRef = collection(db, "users");
+				const q = query(usersRef, where("uid", "==", user.uid));
+
+				const querySnapshot = await getDocs(q);
+				querySnapshot.forEach((doc) => {
+					console.log(doc.id, " => ", doc.data());
+					info = doc.data();
+				});
+
+				setData(info);
+			}
+		});
+		}, []);	
+	
 	return(
 		<Grid container spacing={10}>
-            <Grid item xs={2}>
+            <Grid item xs={3}>
 				<Paper elevation={10}>
 					<center><img src={ProfilePic} alt="Profile" width="190" height="190"/></center>
 				</Paper>
@@ -17,9 +46,9 @@ function ProfileInfo(){
 				 style={{ margin: "0px 0px 8px 0px" }}
 				 >
 					<Typography variant="p" component="p">
-						<center><font size="6"><b>John Doe</b></font></center>
+						<center><font size="6"><b>{data.name}</b></font></center>
 						<br></br>
-						<center>example@example.com</center>
+						<center>{data.email}</center>
 					</Typography>
 				</Paper>
 			</Grid>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,73 +1,11 @@
-import React, {useState, useEffect} from 'react';
 import '../App.css';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import LineChart from '../components/LineChart.jsx';
 import ProfileInfo from '../components/ProfileInfo.jsx';
-import {db} from '../components/Firebase/firebase.js';
-import {
-	query,
-	collection,
-	where,
-	getDocs
-  } from "firebase/firestore";
-import {auth} from '../components/firebase.js';
 
 
-async function getUser() {
-	const user = auth.currentUser;
-	console.log("auth is: ");
-	console.log(auth);
-	const usersRef = collection(db, "users");
-	const q = query(usersRef, where("uid", "==", auth.currentUser.uid));
-	const querySnapshot = await getDocs(q);
-	querySnapshot.forEach((doc) => {
-  		console.log(doc.id, " => ", doc.data());
-	});
-/*	
-	let dates = [];
-	let weights = [];
-	const q2 = query(collection(db, "users", user.uid, "dates"));
-	const querySnapshot2 = getDocs(q2);
-	querySnapshot2.forEach((doc) => {
-   		const date = doc.id;
-   		dates.push(date);
-   		const weight = doc.data().weight;
-   		weights.push(weight);
-	});
-	console.log('dates: ', dates);
-	console.log('weights: ', weights); 
-	return ({"dates": dates,
-         	 "weights": weights,
-        	}); 
-*/
-}
-
-const Profile = () => {
-	//getUser();
-	const [weights, setWeights] = useState([]);
-	useEffect(() => {
-	    const user = auth.currentUser;
-		console.log(user)
-		const q = query(collection(db, "users", user.uid, "dates"));
-		const snapshot = getDocs(q);
-		let dates = [];
-		let wghts = [];
-		snapshot.forEach((rec) => {
-			dates.push(rec.id);
-			wghts.push(rec.data().weight);
-		});
-		
-		const values = {"dates": dates,
-		                "weights": wghts,
-					   };
-		setWeights(values);
-	}, []);
-
-	console.log("plotdata: " + weights);
-	console.log("dates: " + weights.dates);
-	console.log("weights: " + weights.weights);
-
+function Profile() {
 	return (<>
 				<Box padding={5}>
 					<ProfileInfo />
@@ -84,4 +22,5 @@ const Profile = () => {
 				</Container>
 			</>);
 }
+
 export default Profile;
